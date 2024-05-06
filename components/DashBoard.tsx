798bc68e9342f6caa6a87892d14e1fa91ecab847
hiro-551010 <hiro551010@gmail.com>
2024-05-06 23:27:25 +0900

    セクション12

--- components/DashBoard.tsx
+++ components/DashBoard.tsx
@@ -6,11 +6,13 @@ import { Spinner } from './Spinner'
 import { UserProfile } from './UserProfile'
 import { useQueryClient } from 'react-query'
 import useStore from '../store'
+import { Notification } from './Notification'
 
 export const DashBoard: FC = () => {
     // supabaseのAPIを使用してログアウトする関数
     const queryClient = useQueryClient()
     const resetProfile = useStore((state) => state.resetEditedProfile)
+    const resetNotice = useStore((state) => state.resetEditedNotice)
     const signOut = () => {
         resetProfile()
         supabase.auth.signOut()
@@ -24,16 +26,29 @@ export const DashBoard: FC = () => {
                 className="my-6 h-6 w-6 cursor-pointer text-blue-500"
                 onClick={signOut}
             />
-            <div className='flex flex-col items-center'>
-                <ErrorBoundary
-                    fallback={
-                        <ExclamationCircleIcon className='my-5 h-10 w-10 test-pink-500' />
-                    }
-                >
-                    <Suspense fallback={<Spinner />}>
-                        <UserProfile />
-                    </Suspense>
-                </ErrorBoundary>
+            <div className='grid grid-cols-2 gap-4'>
+                <div className='flex flex-col items-center'>
+                    <ErrorBoundary
+                        fallback={
+                            <ExclamationCircleIcon className='my-5 h-10 w-10 test-pink-500' />
+                        }
+                    >
+                        <Suspense fallback={<Spinner />}>
+                            <UserProfile />
+                        </Suspense>
+                    </ErrorBoundary>
+                </div>
+                <div className='flex flex-col items-center'>
+                    <ErrorBoundary
+                        fallback={
+                            <ExclamationCircleIcon className='my-5 h-10 w-10 test-pink-500' />
+                        }
+                    >
+                        <Suspense fallback={<Spinner />}>
+                            <Notification />
+                        </Suspense>
+                    </ErrorBoundary>
+                </div>
             </div>
         </>
     )
